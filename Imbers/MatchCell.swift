86c00b022f31e0f2f028dbd599437dd807a5d875
Dimitar Valchanov <cerrutito@gmail.com>
2014-10-26 08:22:03 +0200

    Show avatar picture of each match

--- Imbers/MatchCell.swift
+++ Imbers/MatchCell.swift
@@ -24,6 +24,7 @@ class MatchCell: UITableViewCell {
   }
   
   @IBOutlet weak var matchName: UILabel!
+  @IBOutlet weak var avatarImageView: UIImageView!
   
   override func translatesAutoresizingMaskIntoConstraints() -> Bool {
     return true
@@ -34,6 +35,20 @@ class MatchCell: UITableViewCell {
   }
   
   private func loadPicture() {
-    
+    self.changeImage(avatar)
+  }
+  
+  private func changeImage(url: String) {
+    var nsurl: NSURL = NSURL.URLWithString(url)
+    var imageRequest: NSURLRequest = NSURLRequest(URL: nsurl)
+    NSURLConnection.sendAsynchronousRequest(imageRequest,
+      queue: NSOperationQueue.mainQueue(),
+      completionHandler:{response, data, error in
+        let base64String = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.fromRaw(0)!)
+        let decodedData = NSData(base64EncodedString: base64String, options: NSDataBase64DecodingOptions.fromRaw(0)!)
+        var decodedimage = UIImage(data: decodedData)
+        
+        self.avatarImageView.image = decodedimage as UIImage
+    })
   }
 }
--- Podfile
+++ Podfile
@@ -3,4 +3,3 @@ platform :ios, '7.0'
 pod "Facebook-iOS-SDK", "~> 3.19"
 pod "AFNetworking", "~> 2.0"
 pod "SSKeychain"
-pod "SDWebImage"
